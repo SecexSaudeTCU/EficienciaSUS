133055d4da811c5b59c03eb2fc623f299ba7e7ca
ericlbc <ericlb@tcu.gov.br>
2020-08-03 20:04:28 -0300

    Update prepara_dados.py

--- scripts/prepara_dados.py
+++ scripts/prepara_dados.py
@@ -17,6 +17,33 @@ import pandas as pd
 
 from consts import ANO, DIRETORIO_DADOS_ORIGINAIS, DIRETORIO_DADOS_INTERMEDIARIOS, UF_REGIAO, NATJUR_ESFERA, NATJUR_TIPO_ADMIN_PUB
 
+
+def insere_coluna_antes_em_df(frame, dicionario, coluna_base, coluna_posicao, coluna_nova):
+
+    # Cria objeto list dos valores do objeto dict dicionario chamados pelas chaves key e...
+    # contidos na coluna coluna_base do objeto pandas DataFrame frame
+    list_valores = [dicionario[key] for key in frame[coluna_base]]
+
+    # Coleta o index do elemento coluna_posicao pertencente ao objeto list...
+    # "frame.columns.to_list()"
+    index_coluna_posicao = frame.columns.to_list().index(coluna_posicao)
+
+    # Insere no objeto pandas DataFrame "frame" a coluna coluna_nova antes da coluna...
+    # coluna_posicao tendo os valores contidos em "list_valores"
+    frame.insert(loc=index_coluna_posicao, column=coluna_nova, value=list_valores)
+
+
+# Retorna valor categórico em faixa de acordo com o valor de "num_leitos"
+def retorna_faixa_leitos(num_leitos):
+    if num_leitos < 1: return 'NA'
+    elif num_leitos < 26: return '1 a 25'
+    elif num_leitos < 51: return '26 a 50'
+    elif num_leitos < 201: return '101 a 200'
+    elif num_leitos < 301: return '201 a 300'
+    return 'Mais de 300'
+
+
+
 if __name__ == '__main__':
 
     # Coleta path mais nome da pasta de dados em formato zip (que contém um único arquivo "csv")
@@ -37,104 +64,116 @@ if __name__ == '__main__':
     # Deleta arquivo de nome "arquivo_dados"
     os.unlink(arquivo_dados)
 
-    # Adicionar coluna REGIAO antes da coluna UF
-    regioes = [UF_REGIAO[uf] for uf in df_tudo.UF]
-    pos_col_antes_uf = df_tudo.columns.to_list().index('UF')
-    df_tudo.insert(loc=pos_col_antes_uf, column='REGIAO', value=regioes)
+    # Insere a coluna REGIAO antes da coluna UF em "df_tudo" usando o objeto dict "UF_REGIAO"
+    insere_coluna_antes_em_df(df_tudo, UF_REGIAO, 'UF', 'UF', 'REGIAO')
 
-    # Adiciona coluna ESFERA depois da coluna TIPO
-    esfera_federativa = [NATJUR_ESFERA[nat_jur] for nat_jur in df_tudo.NAT_JURIDICA]
-    pos_col_antes_tipo = df_tudo.columns.to_list().index('TIPO')
-    df_tudo.insert(loc=pos_col_antes_tipo, column='ESFERA_FEDERATIVA', value=esfera_federativa)
+    # Insere a coluna ESFERA_FEDERATIVA antes da coluna TIPO em "df_tudo" usando o objeto...
+    # dict "NATJUR_ESFERA"
+    insere_coluna_antes_em_df(df_tudo, NATJUR_ESFERA, 'NAT_JURIDICA', 'TIPO', 'ESFERA_FEDERATIVA')
 
-    # Adiciona coluna TIPO_ADMIN_PUB depois da coluna TIPO
-    pos_col_antes_tipo = df_tudo.columns.to_list().index('TIPO')
-    tipo_admin_pub = [NATJUR_TIPO_ADMIN_PUB[nat_jur] for nat_jur in df_tudo.NAT_JURIDICA]
-    df_tudo.insert(loc=pos_col_antes_tipo, column='TIPO_ADMIN_PUB', value=tipo_admin_pub)
+    # Insere a coluna TIPO_ADMIN_PUB antes da coluna TIPO em "df_tudo" usando o objeto dict...
+    # "NATJUR_TIPO_ADMIN_PUB"
+    insere_coluna_antes_em_df(df_tudo, NATJUR_TIPO_ADMIN_PUB, 'NAT_JURIDICA', 'TIPO', 'TIPO_ADMIN_PUB')
 
+    # Coleta o index do elemento NAT_JURIDICA pertencente ao objeto list "df_tudo.columns.to_list()"
+    pos_coluna_nat_jur = df_tudo.columns.to_list().index('NAT_JURIDICA')
+    # Aplica a função "retorna_faixa_leitos" a cada elemento da coluna CNES_LEITOS_SUS de "df_tudo"
+    faixa_leitos = df_tudo['CNES_LEITOS_SUS'].apply(retorna_faixa_leitos)
+    # Insere em "df_tudo" a coluna FAIXA_LEITOS antes da coluna NAT_JURIDICA tendo os valores...
+    # contidos em "faixa_leitos"
+    df_tudo.insert(loc=pos_coluna_nat_jur, column='FAIXA_LEITOS', value=faixa_leitos)
 
-    # Adicionar coluna FAIXA_LEITOS depois de NAT_JURIDICA
-    def retorna_faixa_leitos(num_leitos):
-        if num_leitos < 1: return 'NA'
-        if num_leitos < 26: return '1 a 25'
-        if num_leitos < 51: return '26 a 50'
-        if num_leitos < 201: return '101 a 200'
-        if num_leitos < 301: return '201 a 300'
-        return 'Mais de 300'
-
-
-    pos_depois_nat_jur = pos_col_antes_tipo = df_tudo.columns.to_list().index('NAT_JURIDICA')
-    faixa_leitos = df_tudo.CNES_LEITOS_SUS.apply(retorna_faixa_leitos)
-    df_tudo.insert(loc=pos_depois_nat_jur, column='FAIXA_LEITOS', value=faixa_leitos)
-
-    # Substiui nans pelo valor zero nos valores da produção do SIA e do SIH
-    df_tudo.VALOR_SIA = df_tudo.VALOR_SIA.fillna(value=0)
-    df_tudo.VALOR_SIH = df_tudo.VALOR_SIH.fillna(value=0)
+    # Substiui NaN pelo valor zero nos valores da produção do SIA e do SIH, respectivamente
+    df_tudo['VALOR_SIA'].fillna(value=0, inplace=True)
+    df_tudo['VALOR_SIH'].fillna(value=0, inplace=True)
 
     # Mapeamento entre primeiro dígito do código da natureza jurídica e o tipo de natureza jurídica
-    map_nat_jur = {'1': 'Administração Pública', '2': 'Entidades Empresariais', '3': 'Entidades sem Fins Lucrativos',
-                   '4': 'Pessoas Físicas', '5': 'Organizações Internacionais e Outras Instituições Extraterritoriais'}
+    MAP_NAT_JUR = {'1': 'Administração Pública',
+                   '2': 'Entidades Empresariais',
+                   '3': 'Entidades sem Fins Lucrativos',
+                   '4': 'Pessoas Físicas',
+                   '5': 'Organizações Internacionais e Outras Instituições Extraterritoriais'}
 
-    # Adiciona coluna com natureza jurídica simplificada (depois da coluna NATUREZA_JURIDICA)
+    # Coleta o index do elemento NAT_JURIDICA pertencente ao objeto list "df_tudo.columns.to_list()"...
+    # e acresce o inteiro 1
     pos_col_depois_nat_jur = 1 + df_tudo.columns.to_list().index('NAT_JURIDICA')
-    nat_jur_siplificada = [map_nat_jur[n] for n in df_tudo.NAT_JURIDICA.str[0]]
+    # Cria objeto list dos valores do objeto dict map_nat_jur chamados pelas chaves k e...
+    # contidos no primento elemento do objeto string que constitui a coluna NAT_JURIDICA...
+    # de "df_tudo"
+    nat_jur_siplificada = [MAP_NAT_JUR[k] for k in df_tudo['NAT_JURIDICA'].str[0]]
+    # Insere em "df_tudo" a coluna NAT_JURIDICA_SIMPLIFICADA depois da coluna NAT_JURIDICA tendo...
+    # os valores contidos em "nat_jur_simplificada"
     df_tudo.insert(loc=pos_col_depois_nat_jur, column='NAT_JURIDICA_SIMPLIFICADA', value=nat_jur_siplificada)
 
-    # Lista de tipos constantes da coluna TPPREST que serão mantidos
-    # (todos têm ao menos 9 unidades com  mais de 24 leitos).
-    print('Início', df_tudo.shape)
+    print('Início:', df_tudo.shape)
+
+    # Objeto list de tipos constantes da coluna TPPREST que serão mantidos (todos têm ao menos...
+    # 9 unidades com mais de 24 leitos).
     tipos_unidades_mantidas = ['HOSPITAL GERAL', 'HOSPITAL ESPECIALIZADO', 'UNIDADE MISTA', 'PRONTO SOCORRO GERAL',
                                'PRONTO SOCORRO ESPECIALIZADO', 'HOSPITAL/DIA - ISOLADO']
 
     # Mantém somente unidades de caráter hospitalar
-    df_hosp = df_tudo[df_tudo.TIPO.isin(tipos_unidades_mantidas)]
-    print('Após remover não-hospitais', df_hosp.shape)
+    df_hosp = df_tudo[df_tudo['TIPO'].isin(tipos_unidades_mantidas)]
+
+    print('Após remover não-hospitais:', df_hosp.shape)
 
     # Remove unidades não públicas
-    df_hosp_pubs = df_hosp[df_hosp.NAT_JURIDICA_SIMPLIFICADA == 'Administração Pública']
-    print('Após remover não públicas', df_hosp_pubs.shape)
+    df_hosp_pubs = df_hosp[df_hosp['NAT_JURIDICA_SIMPLIFICADA'] == 'Administração Pública']
+
+    print('Após remover entidades não públicas:', df_hosp_pubs.shape)
 
     # Carrega lista de hospitais geridos por OSS de acordo com a lista do IBROSS
     arquivo_lista_ibross = os.path.join(DIRETORIO_DADOS_ORIGINAIS, 'lista_ibross.xlsx')
-    lista_cnes_oss = pd.read_excel(arquivo_lista_ibross).CNES.to_list()
+    # Obtém objeto list dos códigos CNES contidos na coluna CNES
+    lista_cnes_oss = pd.read_excel(arquivo_lista_ibross)['CNES'].to_list()
 
-    # Adiciona coluna indicando se a unidade é gerida por OSS
-    se_gerida_oss = ['SIM' if cnes in lista_cnes_oss else 'NA' for cnes in df_hosp_pubs.CNES_ID]
+    # Coleta o index do elemento TIPO pertencente ao objeto list "df_tudo.columns.to_list()"
     pos_col_apos_tipo_unidade = df_hosp_pubs.columns.to_list().index('TIPO')
+    # Cria objeto list de elementos "SIM" ou "NA" conforme estão em "lista_cnes_oss" ou não
+    se_gerida_oss = ['SIM' if cnes in lista_cnes_oss else 'NA' for cnes in df_hosp_pubs['CNES_ID']]
+    # Insere em "df_tudo" a coluna SE_GERIDA_OSS antes da coluna TIPO tendo os valores...
+    # contidos em "se_gerida_oss"
     df_hosp_pubs.insert(loc=pos_col_apos_tipo_unidade, column='SE_GERIDA_OSS', value=se_gerida_oss)
 
     # Adiciona a coluna com o valor total da produção do hospital, incluindo SIA e SIH
-    pos_col_antes_valor_sia = df_hosp_pubs.columns.to_list().index('VALOR_SIA')
+    # Coleta o index do elemento VALOR_SIA pertencente ao objeto list "df_tudo.columns.to_list()"
+    pos_col_valor_sia = df_hosp_pubs.columns.to_list().index('VALOR_SIA')
+    # Cria objeto pandas Series que é a soma das colunas VALOR_SIA e VALOR_SIH de "df_tudo"
     valor_total = df_hosp_pubs['VALOR_SIA'] + df_hosp_pubs['VALOR_SIH']
-    df_hosp_pubs.insert(loc=pos_col_antes_valor_sia, column='SIA_SIH_VALOR', value=valor_total)
+    # Insere em "df_tudo" a coluna SIA_SIH_VALOR antes da coluna VALOR_SIA tendo os valores...
+    # contidos em "valor_total"
+    df_hosp_pubs.insert(loc=pos_col_valor_sia, column='SIA_SIH_VALOR', value=valor_total)
 
-    # Renomeia e remove colunas
-    df_hosp_pubs = df_hosp_pubs.rename(columns={
-        'CNES_ID': 'CNES',
-        'MUNNOME': 'MUNICIPIO',
-        'ATIVIDADE': 'ATIVIDADE_ENSINO',
-        'CNES_MEDICOS (PELO CNS)': 'CNES_MEDICOS',
-        'CNES_PROFISSIONAIS_ENFERMAGEM (PELO CNS)': 'CNES_PROFISSIONAIS_ENFERMAGEM',
-        'TIPO': 'TIPO_UNIDADE'
-    })
-    df_hosp_pubs = df_hosp_pubs.drop(columns=[
-        'CNES_MEDICOS (PELO NOME)',  # Estamos usando o CNS para determinar o o profissional
-        'CNES_PROFISSIONAIS_ENFERMAGEM (PELO NOME)',  # Idem
-        'GESTAO',  # Talvez esta coluna seja interessante para comparar OSS gerida por Estado x Municio
-        'PRESTADOR',  # Coluna somente com NA,
-        'VALOR_SIA', 'VALOR_SIH', 'QTD_SIA', 'QTD_SIH'
-    ])
+    # Renomeia as colunas mencionadas
+    df_hosp_pubs = df_hosp_pubs.rename(columns={'CNES_ID': 'CNES',
+                                                'MUNNOME': 'MUNICIPIO',
+                                                'ATIVIDADE': 'ATIVIDADE_ENSINO',
+                                                'CNES_MEDICOS (PELO CNS)': 'CNES_MEDICOS',
+                                                'CNES_PROFISSIONAIS_ENFERMAGEM (PELO CNS)': 'CNES_PROFISSIONAIS_ENFERMAGEM',
+                                                'TIPO': 'TIPO_UNIDADE'})
 
-    # Remove hospitais que possuem valor igual a zero para alguma das variáveis utilizadas na DEA
-    print('Removendo linhas com valor zero nas colunas utilizadas para DEA')
+    # Remove as colunas mencionadas
+    df_hosp_pubs = df_hosp_pubs.drop(columns=['CNES_MEDICOS (PELO NOME)',
+                                              'CNES_PROFISSIONAIS_ENFERMAGEM (PELO NOME)',
+                                              'GESTAO',  # Talvez esta coluna seja interessante para comparar OSS gerida por Estado x Municio
+                                              'PRESTADOR',
+                                              'VALOR_SIA',
+                                              'VALOR_SIH',
+                                              'QTD_SIA',
+                                              'QTD_SIH'])
+
+    print('Removendo linhas com valor zero nas colunas utilizadas para DEA...')
+
+    # Objeto list do nome das colunas de dados que são inputs ou outputs da DEA
     colunas_dea = ['CNES_LEITOS_SUS', 'CNES_SALAS', 'CNES_MEDICOS', 'CNES_PROFISSIONAIS_ENFERMAGEM', 'SIA_SIH_VALOR']
+    # Desconsidera hospitais que possuem valor igual a zero para alguma das variáveis utilizadas na DEA
     for coluna in colunas_dea:
         mascara = df_hosp_pubs[coluna] == 0
         df_hosp_pubs = df_hosp_pubs[~mascara]
         print(' * {n} linhas revmovidas por conter zero na coluna {coluna}'.format(n=sum(mascara), coluna=coluna))
-    print('Número de estabelcimentos ao final do processamento: {n}'.format(n=len(df_hosp_pubs)))
+    print('Número de estabelecimentos ao final do processamento: {n}'.format(n=df_hosp_pubs.shape[0]))
 
-    # Verifica se ao final houve algum hospital somatória de proporções de procedimentos inferior a 0.99
+    # Verifica se ao final houve algum hospital com somatória de proporções de procedimentos inferior a 0.99
     idx = df_hosp_pubs.columns.to_list().index('SIA-0101')
     num_com_soma_inferior_a_um = sum(df_hosp_pubs[df_hosp_pubs.columns[idx::]].sum(axis=1) < 0.999)
     assert (num_com_soma_inferior_a_um == 0)
