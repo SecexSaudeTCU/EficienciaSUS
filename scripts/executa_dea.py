35cd929a2ef6fb14d127225b93c2df8f3d4286a3
ericlbc <ericlb@tcu.gov.br>
2020-08-05 09:08:12 -0300

    Update executa_dea.py

--- scripts/executa_dea.py
+++ scripts/executa_dea.py
@@ -1,6 +1,6 @@
 """
-Executa a análise DEA separadaemente em cada cluster e armazena o resultado em planilhas indivudalizadas por cluster
-e em um única planilha consolidada.
+Executa a análise DEA separadamente em cada cluster e armazena o resultado em planilhas
+individualizadas por cluster e em uma única planilha consolidada.
 """
 
 import os
@@ -8,24 +8,26 @@ import os
 import pandas as pd
 
 from pydea_wrapper import ModeloDEA
-from consts import ANO, DIRETORIO_DADOS_INTERMEDIARIOS, DIRETORIO_DADOS_RESULTADOS
+from consts import ANO, DIRETORIO_DADOS, DIRETORIO_RESULTADOS
 
 if __name__ == '__main__':
 
     # Parâmetros do modelo do TCU
     input_categories = ['CNES_LEITOS_SUS', 'CNES_MEDICOS', 'CNES_PROFISSIONAIS_ENFERMAGEM', 'CNES_SALAS']
     output_categories = ['SIA_SIH_VALOR']
-    virtual_weight_restrictions = ['CNES_SALAS >= 0.09', 'CNES_PROFISSIONAIS_ENFERMAGEM >= 0.09',
-                                   'CNES_LEITOS_SUS >= 0.16', 'CNES_MEDICOS >= 0.16']
+    virtual_weight_restrictions = ['CNES_LEITOS_SUS >= 0.16',
+                                   'CNES_MEDICOS >= 0.16'
+                                   'CNES_PROFISSIONAIS_ENFERMAGEM >= 0.09',
+                                   'CNES_SALAS >= 0.09']
 
-    # Instancia modelo
+    # Instancia modelo DEA
     modelo_tcu = ModeloDEA(input_categories, output_categories, virtual_weight_restrictions)
 
-    # Carrega planilha para DEA
-    arquivo_dados_dea = os.path.join(DIRETORIO_DADOS_INTERMEDIARIOS,
-                                     'hosp_pubs_{ANO}_clusterizado.xlsx'.format(ANO=ANO))
-    # arquivo_dados_dea = os.path.join(DIRETORIO_DADOS_INTERMEDIARIOS, 'amostra.xlsx')
+    # Coleta path do arquivo "xlsx" de dados com a coluna de label do cluster
+    arquivo_dados_dea = os.path.join(DIRETORIO_DADOS, 'hosp_pubs_{ANO}_clusterizado.xlsx'.format(ANO=ANO))
+
+    # Lê o arquivo "xlsx" como um objeto pandas DataFrame
     df = pd.read_excel(arquivo_dados_dea)
 
     # Executa DEA
-    modelo_tcu.executa_por_cluster(arquivo_dados_dea, DIRETORIO_DADOS_RESULTADOS, 'CLUSTER')
+    modelo_tcu.executa_por_cluster(arquivo_dados_dea, DIRETORIO_RESULTADOS, 'CLUSTER')
