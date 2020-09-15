833f10bb3da49b36ec26626ff2719a97cf8db881
ericlbc <ericlb@tcu.gov.br>
2020-09-15 09:09:09 -0300

    Atualiza módulo main

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,3 @@
-import obtem_dados_specific
-import obtem_dados_range
 from clusteriza import clusteriza
 from prepara_dados import prepara_dados_specific, prepara_dados_range
 from executa_dea import executa_deas
@@ -11,7 +9,7 @@ def main_specific(DATABASE_URI, year=2019):
     first_year = int(first_period[3:])
     last_year = int(last_period[3:])
 
-    obtem_dados_specific(DATABASE_URI, year)
+    obtem_dados(DATABASE_URI, year)
     clusteriza(year)
     prepara_dados_specific(year)
     executa_dea(year)
@@ -22,7 +20,7 @@ def main_range(DATABASE_URI, first_period='01-2017', last_period='12-2019'):
     first_year = int(first_period[3:])
     last_year = int(last_period[3:])
 
-    obtem_dados_range(DATABASE_URI, first_period, last_period)
+    obtem_dados(DATABASE_URI, first_period, last_period)
     clusteriza(first_year, last_year)
     prepara_dados_range(first_period, last_period)
     executa_deas(first_period, last_period)
@@ -47,8 +45,12 @@ if __name__ == '__main__':
 
     if tipo == '1':
 
+        from obtem_dados_specific import obtem_dados
+
         main_specific(DATABASE_URI, 2019)
 
     else:
+
+        from obtem_dados_range import obtem_dados
 
         main_range(DATABASE_URI, '01-2017', '02-2020')
